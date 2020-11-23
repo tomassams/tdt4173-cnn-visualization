3339037d3d6d9bd16f96cf2422f82aa43f4c9318
Tomas <31186481+tomassams@users.noreply.github.com>
2020-11-23 14:38:33 +0100

    add esoph, cec, pylo classes

--- src/components/InputSelect.jsx
+++ src/components/InputSelect.jsx
@@ -1,13 +1,26 @@
 import { AspectRatio, Image, SimpleGrid } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const InputSelect = ({ onSelect, inputImages }) => {
+    const [selected, setSelected] = useState(null);
+
+    const clickHandler = (i) => {
+        onSelect(i)
+        setSelected(i)
+    }
 
     return (
-        <>
-            <SimpleGrid columns={4} spacing={4}>
-                { inputImages.map(i => (<AspectRatio key={i.id} ratio={1} onClick={() => onSelect(i)}><Image src={i.inputImage.default}  /></AspectRatio>)) }
-            </SimpleGrid>
-        </>
+        <SimpleGrid columns={4} spacing={4}>
+            { inputImages.map(i => (
+                <AspectRatio 
+                    border={selected && selected.id === i.id ? '10px solid red' : ''} 
+                    key={i.id} 
+                    ratio={1} 
+                    onClick={() => clickHandler(i)}
+                >
+                    <Image src={i.inputImage.default} />
+                </AspectRatio>
+                ))}
+        </SimpleGrid>
     )
 }
