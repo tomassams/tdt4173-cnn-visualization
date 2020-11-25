58abf13d0104fa1a89d3472f1128f72c5ee9e69c
Tomas <31186481+tomassams@users.noreply.github.com>
2020-11-25 10:20:57 +0100

    update readme and html-title

--- src/App.js
+++ src/App.js
@@ -25,20 +25,20 @@ function App() {
   return (
     <Box textAlign="center" m="auto" py={4} maxW={[ "100%", "100%", "80%", "1100px" ]} className="App">
       
-      <Heading as="h1" py={4}>CNN Activation Maps</Heading>
+      <Heading as="h1" py={4}>CNN Visualization</Heading>
       
       <Stack spacing={4} maxW="50%" mx="auto" my={4} textAlign="left">
         <Text>
             This website illustrates how a convolutional neural network processes features in an 
             image input. It was made for the final project in the course TDT4173 Machine Learning 
             at NTNU, and uses the CNN model from our <Link href="https://github.com/tomassams/tdt4173-machine-learning-project">project repository</Link>.
-          The dataset is the <Link href="https://datasets.simula.no/kvasir/">Kvasir dataset v2</Link> and contains 8000 images from the gastrointestinal 
-          tract split into eight class labels representing anatomical landmarks, pathological findings
-          or endoscopic features. For simplicity, we have picked one image from each class to visualize 
-          predictions on this page.
+            The dataset is the <Link href="https://datasets.simula.no/kvasir/">Kvasir dataset v2</Link> and contains 8000 images from the gastrointestinal 
+            tract split into eight class labels representing anatomical landmarks, pathological findings
+            or endoscopic features. For simplicity, we have picked one image from each class to visualize 
+            predictions on this page.
         </Text>
         <Text>
-          Activation maps were extracted with <Link href="https://github.com/philipperemy/keract">Keract</Link>
+          Made with the help of <Link href="https://github.com/facebook/create-react-app">Create React App</Link> and <Link href="https://github.com/philipperemy/keract">Keract</Link>
         </Text>
 
         {selected ? <Text>Selected: {selected.name}</Text> : <Text>Please select an image to view activations</Text> }
